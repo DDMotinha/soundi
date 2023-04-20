cc6af699ea0f592b5c67d98aff6d4c5e73315c8d
David Mariano Gimenez Mota <davidmarianogmota@gmail.com>
2023-04-19 23:35:53 -0300

    aaa

--- src/database/database.js
+++ src/database/database.js
@@ -7,7 +7,7 @@ function csvToJson() {
         const headers = lines[0].split(';');
         const result = [];
         for (let i = 1; i < lines.length; i++) {
-          const values = lines[i].split(',');
+          const values = lines[i].split(';');
           const obj = {};
           for (let j = 0; j < headers.length; j++) {
             obj[headers[j]] = values[j];
